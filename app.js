2ec12e632a613ee567db2814a3f8e9520497cc38
Furkan <furkan18388@gmail.com>
2023-04-16 14:26:16 +0300

    post update ve silme işlemleri eklendi

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const ejs = require("ejs");
 const mongoose = require("mongoose");
 const Post = require("./models/Post");
 const app = express();
+const methodOverride = require("method-override");
+const fs = require("fs");
 
 //connect DB
 mongoose.connect("mongodb://localhost/clean-blog-db", {
@@ -18,7 +20,7 @@ app.use(express.static("public"));
 // eklenen verileri json formatında almak için bunlar kullanılır
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(methodOverride("_method", { methods: ["POST", "GET"] }));
 //ROUTES
 app.get("/", async (req, res) => {
   // db deki tüm postları al ve posts değişkenine atayıp index.ejs ye gönder
@@ -47,6 +49,24 @@ app.get("/post", (req, res) => {
 // add action ile gelen verileri db ye Post modeli ile kaydet
 app.post("/add", async (req, res) => {
   await Post.create(req.body);
+  res.redirect("/");
+});
+
+// delete işlemi için
+app.delete("/posts/:id", async (req, res) => {
+  await Post.findByIdAndDelete(req.params.id);
+  res.redirect("/");
+});
+
+// edit işlemi için
+app.get("/edit/:id", async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  res.render("edit", { post });
+});
+
+// update işlemi için
+app.put("/edit/:id", async (req, res) => {
+  await Post.findByIdAndUpdate(req.params.id, req.body);
   res.redirect("/");
 });
 
